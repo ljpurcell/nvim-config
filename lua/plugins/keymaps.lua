1a52c4dfe24d5db01ed56a837a5de5d49f7c45e9
Lyndon Purcell <ljpurcell.dev@gmail.com>
2024-06-23 09:44:48 +1000

    window resizing

--- lua/plugins/keymaps.lua
+++ lua/plugins/keymaps.lua
@@ -6,45 +6,54 @@ local start_insert_if_terminal = function()
 	end
 end
 
+local set = vim.keymap.set
+
+local common_modes = { "n", "i", "v", "t" }
+
 return {
 
 	-- Move around windows
-	vim.keymap.set({ "n", "i", "v", "t" }, "<A-h>", function()
+	set(common_modes, "<A-h>", function()
 		vim.cmd.wincmd("h")
 		start_insert_if_terminal()
 	end),
 
-	vim.keymap.set({ "n", "i", "v", "t" }, "<A-j>", function()
+	set(common_modes, "<A-j>", function()
 		vim.cmd.wincmd("j")
 		start_insert_if_terminal()
 	end),
 
-	vim.keymap.set({ "n", "i", "v", "t" }, "<A-l>", function()
+	set(common_modes, "<A-l>", function()
 		vim.cmd.wincmd("l")
 		start_insert_if_terminal()
 	end),
 
-	vim.keymap.set({ "n", "i", "v", "t" }, "<A-k>", function()
+	set({ "n", "i", "v", "t" }, "<A-k>", function()
 		vim.cmd.wincmd("k")
 		start_insert_if_terminal()
 	end),
 
-	vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" }),
+	set({ "t", "v", "n" }, "<M-,>", "<c-w>5<"),
+	set({ "t", "v", "n" }, "<M-.>", "<c-w>5>"),
+	set({ "t", "v", "n" }, "<M-=>", "<C-W>+"),
+	set({ "t", "v", "n" }, "<M-->", "<C-W>-"),
 
-	vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>"),
+	set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" }),
 
-	vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv"),
-	vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv"),
-	vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]]),
-	vim.keymap.set("n", "<leader>Y", [["+Y]]),
+	set("n", "<Esc>", "<cmd>nohlsearch<CR>"),
 
-	vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" }),
-	vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" }),
-	vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" }),
-	vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" }),
+	set("v", "J", ":m '>+1<CR>gv=gv"),
+	set("v", "K", ":m '<-2<CR>gv=gv"),
+	set({ "n", "v" }, "<leader>y", [["+y]]),
+	set("n", "<leader>Y", [["+Y]]),
 
-	vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>'),
-	vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>'),
-	vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>'),
-	vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>'),
+	set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" }),
+	set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" }),
+	set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" }),
+	set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" }),
+
+	set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>'),
+	set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>'),
+	set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>'),
+	set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>'),
 }
