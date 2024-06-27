2f5c91c510a5d72e11377c1502a8257a0122ca4c
Lyndon Purcell <ljpurcell.dev@gmail.com>
2024-06-28 06:55:33 +1000

    put lazy and opts into own config file

---
+++ lua/config/lazy.lua
@@ -0,0 +1,77 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- General settings
+vim.g.have_nerd_font = true
+vim.opt.number = true
+vim.opt.mouse = "a"
+vim.opt.showmode = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = "yes"
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.inccommand = "split"
+vim.opt.cursorline = true
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 10
+vim.opt.hlsearch = true
+
+-- Setup lazy.nvim
+require("lazy").setup({
+	{ import = "plugins" },
+	{
+		"rebelot/kanagawa.nvim",
+		priority = 1000,
+		config = function()
+			require("kanagawa").setup({
+				colors = {
+					theme = {
+						all = {
+							ui = {
+								bg_gutter = "none",
+							},
+						},
+					},
+				},
+			})
+
+			vim.cmd.colorscheme("kanagawa-dragon")
+		end,
+	},
+}, {
+	change_detection = {
+		notify = false,
+	},
+	ui = {
+		cmd = "⌘",
+		config = "🛠",
+		event = "📅",
+		ft = "📂",
+		init = "⚙",
+		keys = "🗝",
+		plugin = "🔌",
+		runtime = "💻",
+		require = "🌙",
+		source = "📄",
+		start = "🚀",
+		task = "📌",
+		lazy = "💤 ",
+	},
+})
