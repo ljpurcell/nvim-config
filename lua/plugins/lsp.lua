75d06fe46d7b517e697c415f21cdc2de85ffe671
Lyndon Purcell <lyndon.purcell@pathify.com>
2025-08-31 12:00:52 +1000

    Metals/Scala config working

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,44 +8,49 @@ return {
 		{ "folke/neodev.nvim", opts = {} },
 	},
 	config = function()
+		-- Create a shared function for LSP keybindings
+		local function setup_lsp_keymaps(event)
+			local map = function(keys, func, desc)
+				vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+			end
+
+			map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+			map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+			map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+			map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+			map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+			map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+			map("K", vim.lsp.buf.hover, "Hover Documentation")
+			map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+
+			local client = vim.lsp.get_client_by_id(event.data.client_id)
+			if client and client.server_capabilities.documentHighlightProvider then
+				vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+					buffer = event.buf,
+					callback = vim.lsp.buf.document_highlight,
+				})
+
+				vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+					buffer = event.buf,
+					callback = vim.lsp.buf.clear_references,
+				})
+			end
+		end
+
+		-- Make the function globally available so Metals can use it
+		_G.setup_lsp_keymaps = setup_lsp_keymaps
+
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
-			callback = function(event)
-				local map = function(keys, func, desc)
-					vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-				end
-
-				map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-				map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-				map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-				map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-				map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-				map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-				map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-				map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-				map("K", vim.lsp.buf.hover, "Hover Documentation")
-				map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-
-				local client = vim.lsp.get_client_by_id(event.data.client_id)
-				if client and client.server_capabilities.documentHighlightProvider then
-					vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-						buffer = event.buf,
-						callback = vim.lsp.buf.document_highlight,
-					})
-
-					vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-						buffer = event.buf,
-						callback = vim.lsp.buf.clear_references,
-					})
-				end
-			end,
+			callback = setup_lsp_keymaps,
 		})
 
+		-- Rest of your LSP config remains the same...
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
 		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
 		local servers = {
-			-- clangd = {},
+			-- Your existing servers, but remove any Scala-related ones
 			gopls = {
 				analyses = {
 					unusedparams = true,
@@ -78,7 +83,6 @@ return {
 					},
 				},
 			},
-
 			stylua = {},
 			tailwindcss = {},
 			templ = {},
@@ -88,9 +92,7 @@ return {
 		}
 
 		require("mason").setup()
-
 		local ensure_installed = vim.tbl_keys(servers or {})
-
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
 		require("mason-lspconfig").setup({
--- lua/plugins/metals.lua
+++ lua/plugins/metals.lua
@@ -1,19 +1,192 @@
 return {
 	"scalameta/nvim-metals",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope.nvim",
+	},
+	ft = { "scala", "sbt", "java" },
+	opts = function()
 		local metals_config = require("metals").bare_config()
 
-		metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+		-- Use the same capabilities as your main LSP config
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+		metals_config.capabilities = capabilities
 
-		metals_config.init_options.statusBarProvider = "on"
+		-- Use the same telescope-based LSP keymaps from your main LSP config
+		metals_config.on_attach = function(client, bufnr)
+			-- Create a mock event object that matches what your setup_lsp_keymaps expects
+			local mock_event = {
+				buf = bufnr,
+				data = { client_id = client.id },
+			}
 
-		-- Autocmd to attach Metals when opening scala/sbt files
+			-- Call the global function you defined in your LSP config
+			if _G.setup_lsp_keymaps then
+				_G.setup_lsp_keymaps(mock_event)
+				print("LSP keymaps setup complete for Metals")
+			else
+				print("Warning: _G.setup_lsp_keymaps not found")
+			end
+
+			-- Ensure telescope keymaps are available (in case of loading order issues)
+			local telescope_ok, builtin = pcall(require, "telescope.builtin")
+			if telescope_ok then
+				-- Set up the same telescope keymaps from your telescope config, but with Scala-friendly defaults
+				vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp", buffer = bufnr })
+				vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps", buffer = bufnr })
+
+				-- Override find_files to be selective for Scala projects
+				vim.keymap.set("n", "<leader>sf", function()
+					builtin.find_files({
+						no_ignore = true,
+						hidden = true,
+						file_ignore_patterns = {
+							"target/",
+							".bloop/",
+							".metals/",
+							".bsp/",
+							"project/target/",
+							"project/project/",
+							"%.class$",
+							"%.jar$",
+							"%.log$",
+							"node_modules/",
+							".git/",
+						},
+					})
+				end, { desc = "[S]earch [F]iles", buffer = bufnr })
+
+				vim.keymap.set(
+					"n",
+					"<leader>ss",
+					builtin.builtin,
+					{ desc = "[S]earch [S]elect Telescope", buffer = bufnr }
+				)
+				vim.keymap.set(
+					"n",
+					"<leader>sw",
+					builtin.grep_string,
+					{ desc = "[S]earch current [W]ord", buffer = bufnr }
+				)
+
+				-- Override live_grep to be selective for Scala projects
+				vim.keymap.set("n", "<leader>sg", function()
+					builtin.live_grep({
+						additional_args = {
+							"--no-ignore",
+							"--hidden",
+							"--glob",
+							"!target/",
+							"--glob",
+							"!.bloop/",
+							"--glob",
+							"!.metals/",
+							"--glob",
+							"!.bsp/",
+							"--glob",
+							"!project/target/",
+							"--glob",
+							"!project/project/",
+							"--glob",
+							"!*.class",
+							"--glob",
+							"!*.jar",
+							"--glob",
+							"!*.log",
+							"--glob",
+							"!node_modules/",
+							"--glob",
+							"!.git/",
+						},
+					})
+				end, { desc = "[S]earch by [G]rep", buffer = bufnr })
+
+				vim.keymap.set(
+					"n",
+					"<leader>sd",
+					builtin.diagnostics,
+					{ desc = "[S]earch [D]iagnostics", buffer = bufnr }
+				)
+				vim.keymap.set(
+					"n",
+					"<leader>sr",
+					builtin.oldfiles,
+					{ desc = "[S]earch [R]ecent Files", buffer = bufnr }
+				)
+				vim.keymap.set(
+					"n",
+					"<leader><leader>",
+					builtin.buffers,
+					{ desc = "[ ] Find existing buffers", buffer = bufnr }
+				)
+
+				vim.keymap.set("n", "<leader>s.", function()
+					builtin.live_grep({
+						additional_args = function()
+							return {
+								"--hidden",
+								"--no-ignore",
+								"--glob",
+								"!target/",
+								"--glob",
+								"!.bloop/",
+								"--glob",
+								"!.metals/",
+								"--glob",
+								"!.bsp/",
+								"--glob",
+								"!project/target/",
+								"--glob",
+								"!project/project/",
+								"--glob",
+								"!*.class",
+								"--glob",
+								"!*.jar",
+								"--glob",
+								"!*.log",
+								"--glob",
+								"!node_modules/",
+								"--glob",
+								"!.git/",
+							}
+						end,
+						prompt_title = "Live Grep (including hidden files)",
+					})
+				end, { desc = "[S]earch by Grep (Including Hidden and Dot Files)", buffer = bufnr })
+
+				vim.keymap.set("n", "<leader>/", function()
+					builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+						winblend = 10,
+						previewer = false,
+					}))
+				end, { desc = "[/] Fuzzily search in current buffer", buffer = bufnr })
+
+				vim.keymap.set("n", "<leader>s/", function()
+					builtin.live_grep({
+						grep_open_files = true,
+						prompt_title = "Live Grep in Open Files",
+					})
+				end, { desc = "[S]earch [/] in Open Files", buffer = bufnr })
+
+				vim.keymap.set("n", "<leader>sn", function()
+					builtin.find_files({ cwd = vim.fn.stdpath("config") })
+				end, { desc = "[S]earch [N]eovim files", buffer = bufnr })
+			else
+				print("Warning: telescope.builtin not available")
+			end
+		end
+
+		return metals_config
+	end,
+	config = function(self, metals_config)
+		local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
 		vim.api.nvim_create_autocmd("FileType", {
-			pattern = { "scala", "sbt" },
+			pattern = self.ft,
 			callback = function()
 				require("metals").initialize_or_attach(metals_config)
 			end,
+			group = nvim_metals_group,
 		})
 	end,
 }
